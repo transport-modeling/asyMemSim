56bfed3aff005b32397e4ba46f001f71278de25f
djweber96 <47874943+djweber96@users.noreply.github.com>
2019-02-24 11:41:26 -0500

    Gas separation membrane simulation
    One file includes very basic listing of equations that capture the observed phenomena. 
    The second is an ODE solver and plot function for those equations.

---
+++ mem_gas_sep.m
@@ -0,0 +1,58 @@
+function [G]=mem_gas_sep(z,W)    
+    %gas membrane single simulation 
+    %using solution_diffusion model with dense membrane
+    
+    %declare differential variables
+    C_o2 = W(1);
+    C_n2 = W(2);
+    Q = W(3);
+
+    
+    %factors affecting permeabillity(diff*solub):
+    %------solubility into membrane matrix : temperature, concentration (pressure dependant if gas) can
+    %saturate surface after sometime or at some concentration (if rate-limiting)
+    %------diffusion through membrane matrix: temperature, concentration,
+    %    pressure (Maxwell_Stephen coupled diffusion mass and energy transport)
+                                        %Perm
+        %Test cases (dense polymers):  (Barrer)  Selec
+                                      % O2   N2  tivety     
+    % Poly(1-trimethylsilyl-1-propyne) 7600 5400 1.6           /NOTE ABOUT
+    % Poly(dimethylsiloxane)           638  320   2            /STRUCT:
+    % Poly(4-methyl-1-pentene)          30  7.1  4.2           /CREATING_OBJS_
+    % Poly(phenylene oxide)           16.8  3.8  4.4           /params.pres
+    % Ethyl cellulose                 11.2  3.3  3.4           /params.feed
+    % 6FDA-DAF (polyimide)             7.9  1.3  6.2            /like chess
+    % Polysulfone                     1.38 0.239 5.8            /organization
+    % Polyaramid                        3.1 0.46 6.8            /of pieces
+    % Tetrabromo bis polycarbonate      1.4 0.18 7.8
+
+%test polymers permabillity vector
+    pm_o2 = [7600;638;30;16.8;11.2;7.9;1.38;3.1;1.4]*0.000000122198; %convert units from Barrer to mol*M/M^2*hr*bar (STP)
+    pm_n2 = [5400;320;7.1;3.8;3.3;1.3;0.239;0.46;0.18]*0.000000122138;
+    Pm = [pm_o2,pm_n2];
+%define constants
+    ps = 0; %bar
+    r_i = 0.00015; %meter
+    R_o = 0.00025; %meter
+    T = 298; %Lord Kelvin
+    R = 0.083144621; %L bar/mol K
+    pho = 1.0184; %kg/m^3 at 1 bar
+    mu = 1.81*10^-5; %kg/m*s  at 1 bar and 25 deg C
+%define total flow and partial pressures
+    C_T = C_o2+C_n2; %~~2 mol/L at 50 bar
+    P = R*T*C_T; %assuming IG law
+    pf_o2 = P*C_o2/C_T;
+    pf_n2 = P*C_n2/C_T; 
+%flux across membrane
+    J_o2 = (Pm(3,1)/(R_o-r_i))*(pf_o2-ps);
+    J_n2 = (Pm(3,2)/(R_o-r_i))*(pf_n2-ps); 
+%volumetric_flow
+    g3 = -2*pi()*r_i*(J_n2+J_o2)/(C_T*(1+((Q^2*pho)+pi()*r_i^2*mu*4/3)/(C_T*R*pi()^2*r_i^4*T)*(1/(3600*1000*101325))));
+    %g3 = 0;
+%mass_balence on spiral wound unit
+    %[dC_o2]/[dz] =
+    g1 = (-C_o2*g3-2*pi()*r_i*J_o2)/Q; %oxygen concentration change across single fiber
+    %[dC_n2]/[dz] =
+    g2 = (-C_n2*g3-2*pi()*r_i*J_n2)/Q; %nitrogen concentration change across single fiber
+%differential vector  
+    G=[g1;g2;g3]; 
---
+++ run_ODE45.m
@@ -0,0 +1,38 @@
+function [P] = run_ODE45(  )
+ 
+zspan=[0 15];
+
+W0=[0.042378;0.159423;0.4158]; %based on concentraion of air at 5 bar, and Q = [L/hr] but # based on 0.01ft/s through 2450-fibre bundle
+
+[z,W_s]=ode45(@(z,W) mem_gas_sep(z,W),zspan,W0);
+
+P = 2450.*W_s(:,1).*W_s(:,3); %o2
+P1 = 2450.*W_s(:,2).*W_s(:,3); %n2
+P2 = 0.08314*298.*(W_s(:,1)+W_s(:,2)); %delta P
+P3 = 2450*(W_s(:,3)); %volumetric flow rate
+figure(1)
+plot(z,P,'r*');
+title("Oxygen Flow Rate Through Fiber Bundle");
+xlabel("Membrane Length (m)");
+ylabel("O2 Flow Rate (mol/hr)");
+figure(2)
+plot(z,P1,'m*');
+title("Nitrogen Flow Rate Through Fiber Bundle");
+xlabel("Membrane Length (m)");
+ylabel("N2 Flow Rate (mol/hr)");
+figure(3)
+plot(z,P2,'b--O');
+title("Pressure Drop Across Fiber Bore");
+xlabel("Membrane Length (m)");
+ylabel("Pressure (bar)");
+figure(4)
+plot(z,P3,'-O','Color',[0.9290, 0.6940, 0.1250]);
+title("Volumetric Flow Rate Through Fiber Bundle");
+xlabel("Membrane Length (m)");
+ylabel("Volumetric Flow Rate (bar)");
+
+
+end
+
+
+
