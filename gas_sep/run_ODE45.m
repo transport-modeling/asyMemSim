6370d7569b07d42fbdd758df25f0b5fde2e7248d
Dylan <djweber@g.clemson.edu>
2019-02-27 17:07:09 -0500

    Corrected units on volumetric flow graph

--- gas_sep/run_ODE45.m
+++ gas_sep/run_ODE45.m
@@ -1,7 +1,6 @@
 function [P] = run_ODE45(  )
- 
-zspan=[0 5];
-
+ %ode45 solver
+zspan=[0 10];
 W0=[0.042378;0.159423;0.4158]; %based on concentraion of air at 5 bar, and Q = [L/hr] but # based on 0.01ft/s through 2450-fibre bundle
 
 [z,W_s]=ode45(@(z,W) mem_gas_sep(z,W),zspan,W0);
@@ -29,7 +28,7 @@ figure(4)
 plot(z,P3,'-O','Color',[0.9290, 0.6940, 0.1250]);
 title("Volumetric Flow Rate Through Fiber Bundle");
 xlabel("Membrane Length (m)");
-ylabel("Volumetric Flow Rate (bar)");
+ylabel("Volumetric Flow Rate (L/hr)");
 
 
 end
